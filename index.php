a3a95031910e9753677c64453c0c4bf2be01e2a2
allukard996 <allukard8@gmail.com>
2018-03-23 21:13:42 +0200

    Lesson13

--- index.php
+++ index.php
@@ -1,6 +1,31 @@
 <?php
-session_start();
-$_SESSION['test'] = 'Тестовая запись в сессию';
-?>
 
-<a href="session.php"> go to the next page</a>
+try{
+    $pdo = new PDO('mysql:host=localhost;dbname=Anthony;charset=utf8','Anthony','4044059860');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo 'Подключение успешно.  ';
+} catch(PDOException $exception){
+    echo $exception->getMessage();
+}
+$sql = "SELECT * FROM slim_users ORDER BY username";
+$result = $pdo->query($sql);
+
+/*foreach($result as $row) {
+    echo "<p>".$row['id']." | ".$row['username'].'</p>';
+}*/
+while($row = $result->fetch(PDO::FETCH_OBJ)) {
+   // echo "<p>".$row['id']." | ".$row['username'];
+    var_dump($row);
+}
+
+$users = ['user10','user11','user12'];
+$sql = "INSERT INTO slim_users (username) VALUES (:user)";
+$query = $pdo->prepare($sql);
+
+foreach ($users as $user){
+    $query->bindParam('user',$user);
+    $query->execute();
+    $lastID = $pdo->lastInsertId();
+    echo 'Пользвоатель '.$user.' вставлен с id = '.$lastID.'<br>';
+}
+
